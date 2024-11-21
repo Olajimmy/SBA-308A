9427ee9ba740753bd4493695b7cded43d75e4ec4
olajimmy <overdedicated9@gmail.com>
2024-11-21 01:32:13 -0500

    replaced list element with div tags and added some styling

--- index.js
+++ index.js
@@ -7,22 +7,60 @@ console.log("connected!");
 // const list = document.getElementById("myList");
 // list.appendChild(newListItem);
 
+// const data = { key1: "value1", key2: "value2" };
+
+// // fetch("https://dogapi.dog/api/v2/breeds", {
+// //   method: "POST",
+// //   headers: {
+// //     "Content-Type": "application/json",
+// //   },
+// //   body: JSON.stringify(data),
+// // })
+// //   .then((response) => response.json())
+// //   .then((data) => {
+// //     console.log("Success:", data);
+// //   })
+// //   .catch((error) => {
+// //     console.error("Error:", error);
+// //   });
+//new api call=======================================start here================================
+// async function dogs2() {
+//   const response2 = await fetch(`https://dogapi.dog/api/v2/breeds`);
+//   const result2 = await response2.json();
+//   console.log(result2.data);
+//   const details2 = result2.data;
+//   for (i = 0; i < details2.length; i++) {
+//     const newList = document.createElement("div");
+//     newList.textContent = details2[i].attributes.name;
+//   }
+// }
+
+///======================================================================
 async function dogs() {
   const response = await fetch(`https://dogapi.dog/api/v2/breeds`);
   const result = await response.json();
-  const ulTag = document.querySelector("ul");
-  //console.log(result.data);
+  //const ulTag = document.querySelector("ul");
+  console.log(result.data);
   const details = result.data;
   for (i = 0; i < details.length; i++) {
     console.log(details[i].id);
-    const newList = document.createElement("li");
+    const newList = document.createElement("div");
+    newList.style.border = "1px solid black";
+    newList.style.height = "100px";
+    newList.style.width = "100px";
+    newList.style.backgroundColor = "#22779b";
     newList.textContent = details[i].id;
-    const ulList = document.querySelector("ul");
+    //dogs2();
+    const ulList = document.querySelector("div");
+    // main div container
+    ulList.style.border = "1px solid black";
+    ulList.style.display = "flex";
+    //ulList.style.justifyContent = "space-between";
     console.log(ulList);
     ulList.appendChild(newList);
 
-    const list = document.querySelector("li");
-    console.log(list);
+    // const list = document.querySelector("li");
+    // console.log(list);
     //list.textContent = details[i].id;
     // list.setAttribute("value", details.id);
     //document.querySelector("ul").appendChild(list);
